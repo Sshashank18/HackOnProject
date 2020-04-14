ffadb3da5ae1a3e01733f81bc16ccb53284260e4
Bhaikko <siddhartha.pawar17@gmail.com>
2020-04-14 21:13:47 +0530

    Eraser added

--- blackboard/main.js
+++ blackboard/main.js
@@ -3,22 +3,47 @@ let canvas = document.getElementById("canvas"),
     width = canvas.width = window.innerWidth,
     height = canvas.height = window.innerHeight;
 
+let mouseWrapper = document.getElementById("mouseWrapper");
+
 let mouseDown = false;
 let lastX = 0;
 let lastY = 0;
+
+let currentTool = "pen";
 
 canvas.addEventListener("mousemove", event => {
     let x = event.clientX,
         y = event.clientY;
 
+    if (currentTool === "eraser") {
+       mouseWrapper.style.left = x - 10 + "px";
+       mouseWrapper.style.top = y - 10 + "px";
+    }    
     if (mouseDown) {
+        context.save();
+        switch (currentTool) {
+            case "pen":
+                context.beginPath();
+                context.moveTo(lastX, lastY);
+                context.lineTo(x, y);
+                context.stroke();
+                break;
+                
+            case "eraser":
+                context.beginPath();
+                context.fillStyle = "#FFFFFF";
+                context.arc(x, y, 50, 0, Math.PI * 2, false);
+                context.fill();
+                break;
+            default:
+                break;
 
-        context.beginPath();
-        context.moveTo(lastX, lastY);
-        context.lineTo(x, y);
-        context.stroke();
+        }
+
+        context.restore();
+
     }
-    
+
     lastX = x;
     lastY = y;
 
@@ -32,3 +57,9 @@ canvas.addEventListener("mousedown", () => {
 canvas.addEventListener("mouseup", () => {
     mouseDown = false;
 });
+
+let button = document.getElementById("eraserButton");
+
+button.addEventListener("click", () => {
+    currentTool = currentTool === "pen" ? "eraser" : "pen";
+});3
