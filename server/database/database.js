f3d48d0d37733f26c316695472778cdcc7397df9
Shashank Aggarwal <shashankaggarwal13@gmail.com>
2020-04-14 16:48:58 +0530

    Database added

---
+++ server/database/database.js
@@ -0,0 +1,30 @@
+const Sequelize = require('sequelize');
+
+const database=new Sequelize("Account","admin","admin123",{
+    host:"localhost",
+    dialect:"sqlite",
+    storage:"Accounts.db",
+    logging:false,
+});
+
+const Users=database.define('user',{
+    Name:{
+        type:Sequelize.STRING,
+        allowNull:false,
+    },
+    Email:{
+        type:Sequelize.STRING,
+        allowNull:false,
+        unique:true
+    },
+    Password:{
+        type:Sequelize.STRING,
+        allowNull:false
+    },
+    Position:{
+        type:Sequelize.STRING,
+        allowNull:false,
+    }
+});
+
+module.exports={database,Users};
