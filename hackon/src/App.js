b7586ddb9931fb9786c99b9a432d8add9f8e7b71
Bhaikko <siddhartha.pawar17@gmail.com>
2020-04-16 22:53:19 +0530

    Auth page added

--- hackon/src/App.js
+++ hackon/src/App.js
@@ -4,6 +4,8 @@ import { Switch, Route } from 'react-router-dom';
 
 import socketIOClient from 'socket.io-client';
 
+import Login from './apps/Auth/Login';
+
 import TeacherApp from './apps/Teacher/Teacher';
 import StudentApp from './apps/Student/Student';
 
@@ -16,7 +18,8 @@ class App extends Component {
   render() {
     let routes = (
       <Switch>
-        <Route path="/" exact render={props => <TeacherApp {...props} socket={this.socket} />} />
+        <Route path="/" exact component={Login} />
+        <Route path="/teacher" exact render={props => <TeacherApp {...props} socket={this.socket} />} />
         <Route path="/student" exact render={props => <StudentApp {...props} socket={this.socket} />} />
       </Switch>
     );
---
+++ hackon/src/apps/Auth/Login.js
@@ -0,0 +1,55 @@
+import React, { Component } from 'react';
+
+import classes from './Login.css';
+
+class Login extends Component {
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            type: "Teacher"
+        }
+        
+    }
+
+    clickHandler = () => {
+        if (this.state.type === "Teacher") {
+            window.location = "/teacher";
+        } else {
+            window.location = "/student";
+        }
+    }
+
+    selectHandler = event => {
+        // console.log(event.target.value);
+
+        this.setState({
+            type: event.target.value
+        })
+    }
+
+    render() {
+        return (
+            <div className={classes.container} id="container">
+                <form className={classes.credentials} id="credentials">
+                    <h4 className={classes.heads}>Email:</h4>
+                    <input type="email" name="Email" className={classes.Email} id="Email" placeholder="Email Address" required />
+                    <br /><br />
+                    <h4 className={classes.heads}>Password:</h4>
+                    <input type="password" name="Password" placeholder="Password" className={classes.Password} id="Password" required />
+                    <br /><br /><br />
+                    <select className={classes.position} id="position" required onChange={this.selectHandler}>
+                        <option value="Teacher">Teacher</option>
+                        <option value="Student">Student</option>
+                    </select>
+                    <br /><br /><br />
+                    <button className={classes.signupButton} id="signupButton" >Sign Up</button>
+                    <button className={classes.loginButton} id="loginButton" type="submit" onClick={this.clickHandler}>Log in</button>
+                </form>
+            </div>
+        );
+    }
+}
+
+export default Login;
